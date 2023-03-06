6e3017a274a6458028bb29a3f8fa8a3518aaf4dc
Markus Hopf <info@hopf-it.de>
2023-03-06 11:19:12 +0100

    index_minified

--- hopf-it/minify_index_html.sh
+++ hopf-it/minify_index_html.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Reduces the size from 162 kb to 100 kb.
+# Reduces the size from 173 kb to 118 kb.
 # Rename the index_minified.html to index.html and copy to web hosting server.
 #
 # Call with bash
@@ -8,3 +8,7 @@
 
 cp index.html index_minified.html
 sed -i "s/^[ \t]*//g" index_minified.html
+
+cp index_minified.html /D/temp/index.html
+cp Profil_Web_Hopf_EN.pdf /D/temp/Profil_Web_Hopf_EN.pdf
+cp Profil_Web_Hopf.pdf /D/temp/Profil_Web_Hopf.pdf
