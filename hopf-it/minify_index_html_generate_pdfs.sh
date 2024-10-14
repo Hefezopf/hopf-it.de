35fffb5191c5dbc051854bca642a3e25a2ffe24d
Markus Hopf <info@hopf-it.de>
2024-10-14 08:34:52 +0200

    <li>DokumentationenInstallationen</li>

---
+++ hopf-it/minify_index_html_generate_pdfs.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# Reduces the size from 173 kb to 118 kb.
+# Rename the index_minified.html to index.html and copy to web hosting server.
+#
+# Call with bash
+# Call example: ./minify_index_html_generate_pdfs.sh
+
+cp index.html index_minified.html
+
+echo "minify..."
+sed -i "s/^[ \t]*//g" index_minified.html
+
+echo "generate pdfs..."
+# https://github.com/yeokm1/docs-to-pdf-converter
+java -jar docs-to-pdf-converter-1.8.jar -input Profil_Hopf.docx -output Profil_Web_Hopf.pdf
+java -jar docs-to-pdf-converter-1.8.jar -input Profil_Hopf_engl.docx -output Profil_Web_Hopf_EN.pdf
+
+echo "copy index.html + Profil_Web_Hopf*.pdf to D:/temp"
+echo "If warnings, close WORD Docs!"
+
+cp index_minified.html /D/temp/index.html
+cp Profil_Web_Hopf_EN.pdf /D/temp/Profil_Web_Hopf_EN.pdf
+cp Profil_Web_Hopf.pdf /D/temp/Profil_Web_Hopf.pdf
