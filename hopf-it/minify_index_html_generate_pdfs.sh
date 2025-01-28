1f9ea55f06487cc86b220df8b6bd18505bb20810
Markus Hopf <info@hopf-it.de>
2025-01-28 17:06:28 +0100

    update from laptop

--- hopf-it/minify_index_html_generate_pdfs.sh
+++ hopf-it/minify_index_html_generate_pdfs.sh
@@ -16,10 +16,10 @@ echo "generate pdfs..."
 java -jar docs-to-pdf-converter-1.8.jar -input Profil_Hopf.docx -output Profil_Web_Hopf.pdf
 java -jar docs-to-pdf-converter-1.8.jar -input Profil_Hopf_engl.docx -output Profil_Web_Hopf_EN.pdf
 
-echo "copy index.html + Profil_Web_Hopf*.pdf to D:/temp"
+echo "copy index.html + Profil_Web_Hopf*.pdf to C:/temp"
 echo "If warnings, close WORD Docs!"
 echo "Now: Upload with WinSCP..."
 
-cp index_minified.html /D/temp/index.html
-cp Profil_Web_Hopf_EN.pdf /D/temp/Profil_Web_Hopf_EN.pdf
-cp Profil_Web_Hopf.pdf /D/temp/Profil_Web_Hopf.pdf
+cp index_minified.html /C/temp/index.html
+cp Profil_Web_Hopf_EN.pdf /C/temp/Profil_Web_Hopf_EN.pdf
+cp Profil_Web_Hopf.pdf /C/temp/Profil_Web_Hopf.pdf
